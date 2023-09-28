cc53a96ece381fdd1cfe5f4d730ae8706dfa48e6
scifuentes18 <saanti.c8@gmail.com>
2023-09-28 02:02:25 -0300

    Se agrega el proyecto

--- MiProyecto/settings.py
+++ MiProyecto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Aplicaciones.usuario'
+
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'MiProyecto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'Aplicaciones.sqlite3',
     }
 }
 
